e3c835ad7556cdd79986cc42c500c4326293cf0d
MarconZet <25779550+MarconZet@users.noreply.github.com>
2022-12-05 12:57:19 +0100

    reformat

--- maven/src/main/kotlin/org/virtuslab/bazelsteward/maven/MavenRepository.kt
+++ maven/src/main/kotlin/org/virtuslab/bazelsteward/maven/MavenRepository.kt
@@ -8,20 +8,20 @@ import org.virtuslab.bazelsteward.core.library.SimpleVersion
 import org.virtuslab.bazelsteward.core.library.Version
 
 class MavenRepository {
-    suspend fun findVersions(libraries: List<MavenCoordinates>): Map<MavenCoordinates, List<Version>> =
-        withContext(Dispatchers.IO) {
-            coroutineScope {
-                libraries.map { coordinates ->
-                    async {
-                        val versionResult =
-                            Versions.create().withRepositories(Repository.central())
-                                .withModule(Module.of(coordinates.id.group, coordinates.id.artifact))
-                                .versions()
-                        if (versionResult.errors.isNotEmpty())
-                            println(versionResult.errors)
-                        coordinates to versionResult.mergedListings.available.map { SimpleVersion(it) }
-                    }
-                }.awaitAll().toMap()
-            }
-        }
+  suspend fun findVersions(libraries: List<MavenCoordinates>): Map<MavenCoordinates, List<Version>> =
+    withContext(Dispatchers.IO) {
+      coroutineScope {
+        libraries.map { coordinates ->
+          async {
+            val versionResult =
+              Versions.create().withRepositories(Repository.central())
+                .withModule(Module.of(coordinates.id.group, coordinates.id.artifact))
+                .versions()
+            if (versionResult.errors.isNotEmpty())
+              println(versionResult.errors)
+            coordinates to versionResult.mergedListings.available.map { SimpleVersion(it) }
+          }
+        }.awaitAll().toMap()
+      }
+    }
 }
