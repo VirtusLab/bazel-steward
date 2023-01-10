b7d08ba11616a47e608441aa588c7979428d07d3
Mikołaj Kozieł <mkoziel045@gmail.com>
2023-01-10 13:17:28 +0100

    Changes in regex and SemVer parsing

--- core/src/main/kotlin/org/virtuslab/bazelsteward/core/library/SemanticVersion.kt
+++ core/src/main/kotlin/org/virtuslab/bazelsteward/core/library/SemanticVersion.kt
@@ -3,6 +3,7 @@ package org.virtuslab.bazelsteward.core.library
 import arrow.core.Option
 import arrow.core.Some
 import arrow.core.toOption
+import kotlin.reflect.typeOf
 
 data class SemanticVersion(
   val major: Int,
@@ -33,18 +34,29 @@ data class SemanticVersion(
   else 0
 
   companion object {
-    private val semVerRegex =
+    private val canonicalSemVerRegex =
       Regex("""^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?${'$'}""")
+    private val semVerRegex =
+      Regex("""^(0|[1-9]\d*)(?:\.(0|[1-9]\d*))?(?:\.(0|[1-9]\d*))?(?:(?:-|.)?((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?${'$'}""")
 
     fun fromString(value: String): Option<SemanticVersion> {
       return semVerRegex.matchEntire(value).toOption().map {
         val values = it.groupValues
+        val intValues = values.filter { value -> value.toIntOrNull() != null}
+        val noIntValues =  values.filter { value -> value.toIntOrNull() == null}
+//        SemanticVersion(
+//          values[1].toInt(),
+//          values[2].toInt(),
+//          values[3].toInt(),
+//          values[4],
+//          values[5]
+//        )
         SemanticVersion(
-          values[1].toInt(),
-          values[2].toInt(),
-          values[3].toInt(),
-          values[4],
-          values[5]
+          intValues.getOrNull(1)?.toInt() ?: 0,
+          intValues.getOrNull(2)?.toInt() ?: 0,
+          intValues.getOrNull(3)?.toInt() ?: 0,
+          noIntValues.getOrNull(2) ?: "",
+          noIntValues.getOrNull(5) ?: ""
         )
       }
     }
--- core/src/test/kotlin/org/virtuslab/bazelsteward/core/library/SemanticVersionTest.kt
+++ core/src/test/kotlin/org/virtuslab/bazelsteward/core/library/SemanticVersionTest.kt
@@ -7,11 +7,57 @@ import org.junit.jupiter.api.Test
 
 class SemanticVersionTest {
 
+
   @Test
   fun `test fromString`() {
     testSemanticVersion("1.2.3", 1, 2, 3, "", "")
     testSemanticVersion("2.0.0-alpha+beta", 2, 0, 0, "alpha", "beta")
+    testSemanticVersion("1.0.1", 1, 0, 1, "", "")
+    testSemanticVersion("1.0.0-alpha", 1, 0, 0, "alpha", "")
+    testSemanticVersion("1.0.0-alpha.1", 1, 0, 0, "alpha.1", "")
+    testSemanticVersion("1.0.0-0.3.7", 1, 0, 0, "0.3.7", "")
+    testSemanticVersion("1.0.0-x.7.z.92", 1, 0, 0, "x.7.z.92","")
+    testSemanticVersion("1.0.0-alpha+001", 1, 0, 0, "alpha", "001")
+    testSemanticVersion("1.0.0+20130313144700", 1, 0, 0, "", "20130313144700")
+    testSemanticVersion("1.0.0-beta+exp.sha.5114f85", 1, 0, 0, "beta", "exp.sha.5114f85")
+    testSemanticVersion("1.0.0-alpha", 1, 0, 0, "alpha", "")
+    testSemanticVersion("1.0.0-alpha.1", 1, 0, 0, "alpha.1", "")
+    testSemanticVersion( "1.0.0-alpha.beta", 1, 0, 0, "alpha.beta", "")
+    testSemanticVersion("1.0.0-beta", 1, 0, 0, "beta", "")
+    testSemanticVersion("1.0.0-beta.2", 1, 0, 0, "beta.2", "")
+    testSemanticVersion("1.0.0-beta.11",1, 0, 0, "beta.11", "")
+    testSemanticVersion("1.0.0-rc.1", 1, 0, 0, "rc.1", "")
+    testSemanticVersion("1.0.0", 1, 0, 0, "", "")
+  }
+
+  @Test
+  fun `test fromString wrong versions`() {
     SemanticVersion.fromString("1.2") shouldBe None
+    SemanticVersion.fromString("1") shouldBe None
+    SemanticVersion.fromString("7.9-628") shouldBe None
+    SemanticVersion.fromString("2022.11.29.0.1-api-version-222") shouldBe None
+    SemanticVersion.fromString("1!1.0") shouldBe None
+    SemanticVersion.fromString("1.0.0b1") shouldBe None
+
+    SemanticVersion.fromString("1.0a1-SNAPSHOT") shouldBe None
+    SemanticVersion.fromString("1.0-alpha1") shouldBe None
+    SemanticVersion.fromString("1.0beta1-SNAPSHOT") shouldBe None
+    SemanticVersion.fromString("1.0-b2") shouldBe None
+    SemanticVersion.fromString("1.0-beta3.SNAPSHOT") shouldBe None
+    SemanticVersion.fromString("1.0-beta3") shouldBe None
+    SemanticVersion.fromString("1.0-milestone1-SNAPSHOT") shouldBe None
+    SemanticVersion.fromString("1.0-m2") shouldBe None
+    SemanticVersion.fromString("1.0-rc1-SNAPSHOT") shouldBe None
+    SemanticVersion.fromString("1.0-cr1") shouldBe None
+    SemanticVersion.fromString("1.0-SNAPSHOT") shouldBe None
+    SemanticVersion.fromString("1.0") shouldBe None
+    SemanticVersion.fromString("1.0-sp") shouldBe None
+    SemanticVersion.fromString("1.0-a") shouldBe None
+    SemanticVersion.fromString("1.0-RELEASE") shouldBe None
+    SemanticVersion.fromString("1.0-whatever") shouldBe None
+    SemanticVersion.fromString("1.0.z") shouldBe None
+    SemanticVersion.fromString("1.0.1.0.0.0.0.0.0.0.0.0.0.0.1") shouldBe None
+    SemanticVersion.fromString("1-1.foo-bar-1-baz-0.1") shouldBe None
   }
 
   private fun testSemanticVersion(
