7743cf47fc1e1b5e413207381865a814d58e3af0
Mikołaj Kozieł <mkoziel045@gmail.com>
2023-01-12 17:16:40 +0100

    Added tests for comapring and changed method of comapring prereleases of two versions

--- core/src/main/kotlin/org/virtuslab/bazelsteward/core/library/SemanticVersion.kt
+++ core/src/main/kotlin/org/virtuslab/bazelsteward/core/library/SemanticVersion.kt
@@ -3,6 +3,7 @@ package org.virtuslab.bazelsteward.core.library
 import arrow.core.Option
 import arrow.core.Some
 import arrow.core.toOption
+import java.util.*
 
 data class SemanticVersion(
   val major: Int,
@@ -29,24 +30,74 @@ data class SemanticVersion(
   else if (this.patch != other.patch)
     this.patch.compareTo(other.patch)
   else if (this.prerelease != other.prerelease)
-    this.prerelease.compareTo(other.prerelease)
+    this.comparePreReleases(this.prerelease, other.prerelease)
   else 0
+
+  private fun comparePreReleases(preRelease: String, other: String): Int{
+    val qualifiers: Map<String, Int> = mapOf(
+      Pair("alpha",1),
+      Pair("beta", 2),
+      Pair("milestone", 3),
+      Pair("rc", 4),
+      Pair("cr", 4),
+      Pair("snapshot", 5),
+      Pair("", 6),
+      Pair("ga", 6),
+      Pair("final", 6),
+      Pair("release", 6),
+      Pair("sp", 7)
+    )
+
+    val firstQualifier: String? = getQualifierForPreRelease(qualifiers, preRelease)
+    val otherQualifier: String? = getQualifierForPreRelease(qualifiers, other)
+
+    val compareQualifiers = when{
+      qualifiers[firstQualifier] == null -> if(qualifiers[otherQualifier] == null) preRelease.compareTo(other) else -1
+      qualifiers[otherQualifier] == null -> 1
+      else -> qualifiers[firstQualifier]!!.compareTo(qualifiers[otherQualifier]!!)
+    }.let {
+      if(it == 0){
+        val preReleaseNoQualifier = getPreReleaseNoQualifier(firstQualifier!!, preRelease)
+        val otherNoQualifier = getPreReleaseNoQualifier(otherQualifier!!, other)
+        preReleaseNoQualifier.compareTo(otherNoQualifier)
+      } else it
+    }
+
+    return compareQualifiers
+  }
+
+  private fun getQualifierForPreRelease(qualifiers: Map<String, Int>, preRelease: String): String?{
+    return when{
+      preRelease.startsWith('a') -> "alpha"
+      preRelease.startsWith('b') -> "beta"
+      preRelease.startsWith('m') -> "milestone"
+      else -> qualifiers.keys.firstOrNull{ preRelease.contains(it) }
+    }
+  }
+
+  private fun getPreReleaseNoQualifier(qualifier: String, preRelease: String): String{
+    return if(preRelease.contains(qualifier)){
+      preRelease.replaceFirst(qualifier,"")
+    } else {
+      preRelease.replaceFirst(qualifier.first().toString(),"")
+    }
+  }
 
   companion object {
     private val canonicalSemVerRegex =
       Regex("""^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?${'$'}""")
     private val semVerRegex =
-      Regex("""^(0|[1-9]\d*)(?:\.(0|[1-9]\d*))?(?:\.(0|[1-9]\d*))?(?:[-.]?((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?${'$'}""")
+        Regex("""^(?<majorRegex>0|[1-9]\d*)(?:[.-](?<minorRegex>(0|[1-9]\d*)))?(?:[.-]?(?<patchRegex>(0|[1-9]\d*)?))(?:[-.]?(?<preReleaseRegex>((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)))?(?:\+(?<buildMetaDataRegex>([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*)))?${'$'}""")
 
     fun fromString(value: String): Option<SemanticVersion> {
       return semVerRegex.matchEntire(value).toOption().map { matchResult ->
-        val values = matchResult.groupValues
+        val values = matchResult.groups as MatchNamedGroupCollection
         SemanticVersion(
-          values.getOrNull(1)?.toIntOrNull() ?: 0,
-          values.getOrNull(2)?.toIntOrNull() ?: 0,
-          values.getOrNull(3)?.toIntOrNull() ?: 0,
-          values.getOrNull(4) ?: "",
-          values.getOrNull(5) ?: ""
+          values["majorRegex"]?.value?.toIntOrNull() ?: 0,
+          values["minorRegex"]?.value?.toIntOrNull() ?: 0,
+          values["patchRegex"]?.value?.toIntOrNull() ?: 0,
+          values["preReleaseRegex"]?.value ?: "",
+          values["buildMetaDataRegex"]?.value ?: "",
         )
       }
     }
--- core/src/test/kotlin/org/virtuslab/bazelsteward/core/library/VersionTest.kt
+++ core/src/test/kotlin/org/virtuslab/bazelsteward/core/library/VersionTest.kt
@@ -1,55 +1,73 @@
 package org.virtuslab.bazelsteward.core.library
 
 import arrow.core.None
-import io.kotest.matchers.shouldBe
+import arrow.core.compareTo
+import io.kotest.matchers.ints.shouldBeGreaterThan
 import io.kotest.matchers.shouldNotBe
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class VersionTest {
-  private val notSemVerVersions = listOf(
-    "1.0a1-SNAPSHOT+whatever",
+  private val mavenVersions = listOf(
     "1.0+whatever",
-    "1.final",
+    "1.0a1-SNAPSHOT+whatever",
     "1.final-2a",
-    "1.0-0",
+    "1.final",
     "1.final+whatever",
-    "1.0-alpha1",
+    "1.0-0",
     "1.0beta1-SNAPSHOT",
-    "1.0-b2",
+    "1.0-alpha1",
     "1.0-beta3.SNAPSHOT",
-    "1.0-beta3",
+    "1.0-b2",
     "1.0-milestone1-SNAPSHOT",
-    "1.0-m2",
+    "1.0-beta3",
     "1.0-rc1-SNAPSHOT",
-    "1.0-cr1",
+    "1.0-m2",
     "1.0-SNAPSHOT",
-    "1.0",
+    "1.0-cr1",
     "1.0-sp",
-    "1.0-a",
+    "1.0",
     "1.0-RELEASE",
-    "1.0-whatever",
+    "1.0-a",
     "1.0.z",
-    "1.0.1.0.0.0.0.0.0.0.0.0.0.0.1",
+    "1.0-whatever",
     "1-1.foo-bar-1-baz-0.1",
+    "1.0.1.0.0.0.0.0.0.0.0.0.0.0.1",
     "2022.11.29.0.1-api-version-222",
     "1.0.0b1",
+    "7.10-final",
     "7.9-628"
   )
 
-  private fun singleArguments(): Stream<String> {
-    return notSemVerVersions.stream()
+  private fun argumentsForCheckVersion(): Stream<String> {
+    return mavenVersions.stream()
   }
 
-//  @Test
   @ParameterizedTest
-  @MethodSource("singleArguments")
-  fun `not SemVer Version`(version: String) {
+  @MethodSource("argumentsForCheckVersion")
+  fun `check version`(version: String) {
       val ver = SimpleVersion(version)
       ver.toSemVer() shouldNotBe None
   }
 
+  private fun argumentsForCompareVersions(): Stream<Arguments> {
+    val listWithArguments: MutableList<Arguments> = mutableListOf()
+    for(i in mavenVersions.indices step 2){
+      listWithArguments.add(Arguments.of(mavenVersions[i], mavenVersions[i+1]))
+    }
+    return listWithArguments.stream()
+  }
+
+  @ParameterizedTest
+  @MethodSource("argumentsForCompareVersions")
+  fun `compare Versions`(first: String, second: String) {
+    val firstVer = SimpleVersion(first).toSemVer()
+    val secondVer = SimpleVersion(second).toSemVer()
+    firstVer.compareTo(secondVer) shouldBeGreaterThan 0
+  }
 }
