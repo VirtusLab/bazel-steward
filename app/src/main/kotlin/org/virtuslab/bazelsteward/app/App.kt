bb88f271becbd258029d5c472844d9c82df53f71
MarconZet <25779550+MarconZet@users.noreply.github.com>
2023-01-20 12:53:43 +0100

    debug more

--- app/src/main/kotlin/org/virtuslab/bazelsteward/app/App.kt
+++ app/src/main/kotlin/org/virtuslab/bazelsteward/app/App.kt
@@ -5,12 +5,15 @@ import mu.KotlinLogging
 import org.virtuslab.bazelsteward.common.GitOperations
 
 private val logger = KotlinLogging.logger {}
+
 class App(private val ctx: Context) {
   suspend fun run() {
     ctx.gitOperations.checkoutBaseBranch()
     val definitions = ctx.bazelFileSearch.buildDefinitions
     logger.debug { definitions.map { it.first } }
     val mavenData = ctx.mavenDataExtractor.extract()
+    logger.debug { mavenData.repositories }
+    logger.debug { mavenData.dependencies.map { it.id.name + " " + it.version.value } }
     val availableVersions = ctx.mavenRepository.findVersions(mavenData)
     val updateSuggestions =
       availableVersions.map {
@@ -20,6 +23,7 @@ class App(private val ctx: Context) {
     changeSuggestions.forEach { change ->
       val branch = GitOperations.Companion.fileChangeSuggestionToBranch(change)
       if (!ctx.gitHostClient.checkIfPrExists(branch)) {
+        logger.info { "Creating branch ${branch.name}" }
         ctx.gitOperations.createBranchWithChange(change)
         if (ctx.config.pushToRemote) {
           ctx.gitOperations.pushBranchToOrigin(branch)
