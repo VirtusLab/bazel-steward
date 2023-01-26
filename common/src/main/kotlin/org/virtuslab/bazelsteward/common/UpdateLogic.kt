58674cdb1188c19c09ee0f26b1dae13cd4f02787
azdrojowa123 <33667003+azdrojowa123@users.noreply.github.com>
2023-01-26 12:23:15 +0100

    Version update logic (#90)
    * update logic tests
    
    * configurable bumping strategy
    
    Co-authored-by: azdrojowa123 <azdrojowa@gmail.com>

--- common/src/main/kotlin/org/virtuslab/bazelsteward/common/UpdateLogic.kt
+++ common/src/main/kotlin/org/virtuslab/bazelsteward/common/UpdateLogic.kt
@@ -1,8 +1,11 @@
 package org.virtuslab.bazelsteward.common
 
 import org.virtuslab.bazelsteward.config.BazelStewardConfig
+import org.virtuslab.bazelsteward.config.BumpingStrategy
+import org.virtuslab.bazelsteward.config.ConfigEntry
 import org.virtuslab.bazelsteward.core.library.Library
 import org.virtuslab.bazelsteward.core.library.LibraryId
+import org.virtuslab.bazelsteward.core.library.SemanticVersion
 import org.virtuslab.bazelsteward.core.library.Version
 import org.virtuslab.bazelsteward.core.library.VersioningSchema
 import org.virtuslab.bazelsteward.core.library.VersioningType
@@ -15,27 +18,41 @@ class UpdateLogic(private val bazelStewardConfig: BazelStewardConfig) {
     library: Library<Lib>,
     availableVersions: List<Version>
   ): UpdateSuggestion<Lib>? {
-    val versioningSchemaForLibrary = getVersioningForLibrary(library)
-    return library.version.toSemVer(versioningSchemaForLibrary)
-      ?.takeIf { version -> version.prerelease.isBlank() && version.buildmetadata.isBlank() }
+    val (versioningSchema, bumpingStrategy) = getConfigurableSetupForLibrary(library)
+
+    fun maxAvailableVersion(filterVersionComponent: (a: SemanticVersion) -> Boolean): SemanticVersion? =
+      availableVersions
+        .mapNotNull { it.toSemVer(versioningSchema) }
+        .filter { it.prerelease.isBlank() && filterVersionComponent(it) }
+        .maxOrNull()
+
+    return library.version.toSemVer(versioningSchema)
+      ?.takeIf { version -> version.prerelease.isBlank() }
       ?.let { version ->
-        availableVersions
-          .asSequence()
-          .mapNotNull { it.toSemVer(versioningSchemaForLibrary) }
-          .filter { it.major == version.major }
-          .filter { it.buildmetadata.isBlank() && it.prerelease.isBlank() }
-          .filter { version < it }
-          .maxOrNull()?.let { nextVersion -> UpdateSuggestion(library, nextVersion) }
+        val maxPatch = maxAvailableVersion { a -> a.major == version.major && a.minor == version.minor }?.takeIf { it.patch > version.patch }
+        val maxMinor = maxAvailableVersion { a -> a.major == version.major }?.takeIf { it.minor > version.minor }
+        val maxMajor = maxAvailableVersion { _ -> true }?.takeIf { it.major > version.major }
+        val nextLibrary = when (bumpingStrategy) {
+          BumpingStrategy.DEFAULT -> maxPatch ?: maxMinor ?: maxMajor
+          BumpingStrategy.LATEST -> maxMajor ?: maxMinor ?: maxPatch
+        }
+        nextLibrary?.let { UpdateSuggestion(library, it) }
       }
   }
 
-  private fun <Lib : LibraryId> getVersioningForLibrary(library: Library<Lib>): VersioningSchema {
+  private fun getConfigEntryFromConfigs(libraryId: MavenLibraryId, configs: List<ConfigEntry>): ConfigEntry? =
+    configs.firstOrNull { it.group == libraryId.group && it.artifact == libraryId.artifact }
+      ?: configs.firstOrNull { it.group == libraryId.group && it.artifact == null }
+      ?: configs.firstOrNull { it.group == null && it.artifact == null }
+
+  private fun <Lib : LibraryId> getConfigurableSetupForLibrary(library: Library<Lib>): Pair<VersioningSchema, BumpingStrategy> {
     return when (val libraryId = library.id) {
       is MavenLibraryId -> {
-        val matchingDependencies = bazelStewardConfig.maven.ruledDependencies.filter { it.id == libraryId }
-        matchingDependencies.firstOrNull()?.versioning ?: VersioningSchema(VersioningType.LOOSE.name)
+        val versioningForDependency = getConfigEntryFromConfigs(libraryId, bazelStewardConfig.maven.configs.filter { it.versioning != null })
+        val bumpingForDependency = getConfigEntryFromConfigs(libraryId, bazelStewardConfig.maven.configs.filter { it.bumping != null })
+        Pair(versioningForDependency?.versioning ?: VersioningSchema(VersioningType.LOOSE.name), bumpingForDependency?.bumping ?: BumpingStrategy.DEFAULT)
       }
-      else -> VersioningSchema(VersioningType.LOOSE.name)
+      else -> Pair(VersioningSchema(VersioningType.LOOSE.name), BumpingStrategy.DEFAULT)
     }
   }
 }
---
+++ common/src/main/test/kotlin/org/virtuslab/bazelsteward/common/UpdateLogicTest.kt
@@ -0,0 +1,120 @@
+package common.src.main.test.kotlin.org.virtuslab.bazelsteward.common
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.virtuslab.bazelsteward.common.UpdateLogic
+import org.virtuslab.bazelsteward.config.BazelStewardConfig
+import org.virtuslab.bazelsteward.config.BumpingStrategy
+import org.virtuslab.bazelsteward.config.ConfigEntry
+import org.virtuslab.bazelsteward.config.MavenConfig
+import org.virtuslab.bazelsteward.core.library.SimpleVersion
+import org.virtuslab.bazelsteward.core.library.VersioningSchema
+import org.virtuslab.bazelsteward.core.library.VersioningType
+import org.virtuslab.bazelsteward.maven.MavenCoordinates
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UpdateLogicTest {
+
+  private val availableVersions = listOf(
+    SimpleVersion("2.0.1"),
+    SimpleVersion("2.0.2"),
+    SimpleVersion("2.0.3-alpha"),
+    SimpleVersion("2.1.0"),
+    SimpleVersion("3.2.1+beta"),
+    SimpleVersion("2.2.1"),
+    SimpleVersion("2.3.2+beta"),
+    SimpleVersion("2.2.0"),
+    SimpleVersion("2.3.0+beta"),
+    SimpleVersion("3.0.1"),
+    SimpleVersion("3.2.0+beta"),
+    SimpleVersion("3.1.0"),
+    SimpleVersion("2.1.6+beta"),
+    SimpleVersion("2.3.3+beta"),
+    SimpleVersion("4.0.1-alpha"),
+    SimpleVersion("4.0.2"),
+    SimpleVersion("2.2.8+beta"),
+  )
+
+  @ParameterizedTest
+  @MethodSource("argumentsForSelectUpdateDefault")
+  fun `should selectUpdate test with default bumping strategy`(version: String, suggestion: String?) {
+    val coordinates = MavenCoordinates.of("group", "artifact", version)
+    val updateSuggestion = UpdateLogic(BazelStewardConfig()).selectUpdate(coordinates, availableVersions)
+    Assertions.assertThat(updateSuggestion?.suggestedVersion?.value).isEqualTo(suggestion)
+  }
+
+  private fun argumentsForSelectUpdateDefault(): List<Arguments> = listOf(
+    Arguments.of("2.0.0", "2.0.2"),
+    Arguments.of("2.0.1+beta", "2.0.2"),
+    Arguments.of("2.0.0-alpha", null),
+    Arguments.of("2.0.2+beta", "2.3.3+beta"),
+    Arguments.of("2.0.2", "2.3.3+beta"),
+    Arguments.of("2.1.0+beta", "2.1.6+beta"),
+    Arguments.of("2.3.3", "4.0.2"),
+    Arguments.of("2.3.2+beta", "2.3.3+beta"),
+    Arguments.of("3.0.1", "3.2.1+beta"),
+    Arguments.of("3.0.0+beta", "3.0.1"),
+    Arguments.of("4.0.0-alpha", null),
+    Arguments.of("4.0.0", "4.0.2"),
+  )
+
+  @ParameterizedTest
+  @MethodSource("argumentsForSelectUpdateLatest")
+  fun `should selectUpdate test with latest bumping strategy`(version: String, suggestion: String?) {
+    val coordinates = MavenCoordinates.of("group", "artifact", version)
+    val mavenConfig = MavenConfig(
+      listOf(
+        ConfigEntry(null, null, null, BumpingStrategy.LATEST)
+      )
+    )
+    val updateSuggestion = UpdateLogic(BazelStewardConfig(mavenConfig)).selectUpdate(coordinates, availableVersions)
+    Assertions.assertThat(updateSuggestion?.suggestedVersion?.value).isEqualTo(suggestion)
+  }
+
+  private fun argumentsForSelectUpdateLatest(): List<Arguments> = listOf(
+    Arguments.of("2.0.0", "4.0.2"),
+    Arguments.of("2.0.1+beta", "4.0.2"),
+    Arguments.of("2.0.0-alpha", null),
+    Arguments.of("2.0.2+beta", "4.0.2"),
+    Arguments.of("2.0.2", "4.0.2"),
+    Arguments.of("2.1.0+beta", "4.0.2"),
+    Arguments.of("2.3.3", "4.0.2"),
+    Arguments.of("2.3.2+beta", "4.0.2"),
+    Arguments.of("3.0.1", "4.0.2"),
+    Arguments.of("3.0.0+beta", "4.0.2"),
+    Arguments.of("4.0.0-alpha", null),
+    Arguments.of("4.0.0", "4.0.2"),
+  )
+
+  @ParameterizedTest
+  @MethodSource("argumentsForSelectUpdateMix")
+  fun `should selectUpdate test with mixed bumping strategies`(group: String, artifact: String, version: String, suggestion: String?) {
+    val coordinates = MavenCoordinates.of(group, artifact, version)
+    val bazelStewardConfig = BazelStewardConfig(
+      MavenConfig(
+        listOf(
+          ConfigEntry("g2", "a1", VersioningSchema(VersioningType.LOOSE.name), BumpingStrategy.DEFAULT),
+          ConfigEntry("g3", "a3", VersioningSchema(VersioningType.LOOSE.name), BumpingStrategy.DEFAULT),
+          ConfigEntry("g1", null, null, BumpingStrategy.DEFAULT),
+          ConfigEntry("g2", null, null, BumpingStrategy.LATEST),
+          ConfigEntry(null, null, null, BumpingStrategy.LATEST),
+        ),
+      )
+    )
+    val updateSuggestion = UpdateLogic(bazelStewardConfig).selectUpdate(coordinates, availableVersions)
+    Assertions.assertThat(updateSuggestion?.suggestedVersion?.value).isEqualTo(suggestion)
+  }
+
+  private fun argumentsForSelectUpdateMix(): List<Arguments> = listOf(
+    Arguments.of("g1", "a1", "2.0.0", "2.0.2"),
+    Arguments.of("g1", "a4", "2.0.2+beta", "2.3.3+beta"),
+    Arguments.of("g2", "a1", "2.0.2", "2.3.3+beta"),
+    Arguments.of("g2", "a2", "3.0.1", "4.0.2"),
+    Arguments.of("g3", "a1", "2.1.0+beta", "4.0.2"),
+    Arguments.of("g3", "a3", "2.3.2+beta", "2.3.3+beta"),
+    Arguments.of("g3", "a5", "3.0.0+beta", "4.0.2"),
+  )
+}
