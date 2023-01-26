5f9256e677772a4f3bf42d0b0708f2f28678be28
Kamil Doległo <kamilok1965@interia.pl>
2023-01-26 13:23:21 +0100

    Fix tests for new Version Update strategy

--- e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/MavenE2ETest.kt
+++ e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/MavenE2ETest.kt
@@ -13,7 +13,7 @@ class MavenE2ETest : E2EBase() {
     val file = loadTest(tempDir, testResourcePath)
     Main.main(args = arrayOf(file.toString(), "--push-to-remote"))
     val expectedBranches =
-      listOf("arrow-core" to "1.1.5", "arrow-fx-coroutines" to "1.1.5", "bazel" to "5.4.0")
+      listOf("arrow-core" to "1.1.5", "arrow-fx-coroutines" to "1.1.5", "bazel" to "5.3.2")
         .map { "$branchRef/${it.first}/${it.second}" } + masterRef
     checkBranchesWithVersions(tempDir, testResourcePath, expectedBranches)
   }
@@ -23,7 +23,7 @@ class MavenE2ETest : E2EBase() {
     val testResourcePath = "maven/external"
     val file = loadTest(tempDir, testResourcePath)
     Main.main(args = arrayOf(file.toString(), "--push-to-remote"))
-    val expectedBranches = listOf("$branchRef/utilis", "$branchRef/bazel/5.4.0", masterRef)
+    val expectedBranches = listOf("$branchRef/utilis", "$branchRef/bazel/5.3.2", masterRef)
     checkBranchesWithoutVersions(tempDir, testResourcePath, expectedBranches)
   }
 }
