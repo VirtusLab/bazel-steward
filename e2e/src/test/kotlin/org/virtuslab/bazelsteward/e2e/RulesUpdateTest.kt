a1c10ecd62848f95601520b375c8e447a937c2b2
Kamil Doległo <kamilok1965@interia.pl>
2023-02-09 14:06:42 +0100

    Fix tests once more

--- e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/RulesUpdateTest.kt
+++ e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/RulesUpdateTest.kt
@@ -13,7 +13,7 @@ class RulesUpdateTest : E2EBase() {
     val file = loadTest(tempDir, testResourcePath)
     Main.main(args = arrayOf(file.toString(), "--push-to-remote"))
     val expectedBranches =
-      listOf("arrow-core" to "1.1.5", "arrow-fx-coroutines" to "1.1.5", "bazel" to "5.3.2", "rules_jvm_external" to "4.5")
+      listOf("io.arrow-kt/arrow-core" to "1.1.5", "io.arrow-kt/arrow-fx-coroutines" to "1.1.5", "bazel" to "5.3.2", "rules_jvm_external" to "4.5")
         .map { "$branchRef/${it.first}/${it.second}" } + masterRef
 
     checkBranchesWithVersions(tempDir, testResourcePath, expectedBranches)
