57dfd6d7f872da3ea4c25fa73ede37a5cf433253
Mikołaj Kozieł <mkoziel045@gmail.com>
2023-03-10 15:46:09 +0100

    Test

--- e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/rules_update/K8SRulesUpdateTest.kt
+++ e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/rules_update/K8SRulesUpdateTest.kt
@@ -1,8 +1,10 @@
 package org.virtuslab.bazelsteward.e2e.rules_update
 
+import org.junit.jupiter.api.Disabled
 import org.virtuslab.bazelsteward.e2e.E2EBase
 import org.virtuslab.bazelsteward.e2e.RulesUpdateTest
 
+@Disabled("test")
 class K8SRulesUpdateTest : RulesUpdateTest(
   "rules/trivial/rules_k8s",
   E2EBase().expectedBranches(
