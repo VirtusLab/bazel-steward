b971ea096a19f7226dce5eee965289bea9eb6cf0
MarconZet <user.name>
2023-03-30 15:45:09 +0200

    litter fix

--- e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/PostUpdateHookTest.kt
+++ e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/PostUpdateHookTest.kt
@@ -3,6 +3,7 @@ package org.virtuslab.bazelsteward.e2e
 import io.kotest.common.runBlocking
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldNotContain
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import org.virtuslab.bazelsteward.core.common.CommandRunner
@@ -11,6 +12,7 @@ import org.virtuslab.bazelsteward.e2e.fixture.E2EBase
 import java.nio.file.Path
 
 class PostUpdateHookTest : E2EBase() {
+
   @Test
   fun `Post update hook with PR`(@TempDir tempDir: Path) {
     val project = "hook/pr"
@@ -60,6 +62,33 @@ class PostUpdateHookTest : E2EBase() {
         localRoot,
       ).trim() shouldBe "45"
       git.run("log", "-1", "--oneline") shouldContain "Test message"
+      git.checkout(master)
+    }
+
+    val expectedBranches = expectedBranches(
+      "io.arrow-kt/arrow-core" to "1.1.5",
+    )
+    checkBranchesWithVersions(tempDir, project, expectedBranches)
+  }
+
+  @Test
+  fun `Post hook update with maven install pin`(@TempDir tempDir: Path) {
+    val project = "hook/pin"
+    runBazelStewardWith(tempDir, project) {
+      it.withMavenOnly(listOf("1.1.5"))
+    }
+
+    val localRoot = tempDir.resolve("local").resolve(project)
+    val git = GitClient(localRoot)
+    runBlocking {
+      git.checkout("bazel-steward/io.arrow-kt/arrow-core/1.1.5")
+
+      val oldDep = "io.arrow-kt:arrow-core:1.1.2"
+      val newDep = "io.arrow-kt:arrow-core:1.1.5"
+      val contents = localRoot.resolve("maven_install.json").toFile().readLines()
+      contents.forEach { it shouldNotContain oldDep }
+      contents.any { it.contains(newDep) } shouldBe true
+
       git.checkout(master)
     }
 
--- e2e/src/test/resources/hook/commit/generate_litter.sh
+++ e2e/src/test/resources/hook/commit/generate_litter.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env sh
 for dir in rubbish trash garbage
 do
   mkdir $dir
-  for i in {1..5}
+  for i in 1 2 3 4 5
   do
     echo "$i" >$dir/file_$i.txt
   done
