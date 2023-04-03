75fe36459bee5d3810b087dc1e8643bbb22bfe9e
Mikołaj Kozieł <mkoziel045@gmail.com>
2023-04-03 15:24:33 +0200

    Added test cases to BazelRulesHeuristicTest

---
+++ e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/rules/HeuristicUpdateMultipleUrlsVariablesTest.kt
@@ -0,0 +1,11 @@
+package org.virtuslab.bazelsteward.e2e.rules
+
+import org.junit.jupiter.api.Disabled
+import org.virtuslab.bazelsteward.e2e.rules.fixture.BazelRulesHeuristicUpdate
+
+class HeuristicUpdateMultipleUrlsVariablesTest: BazelRulesHeuristicUpdate(
+  "rules/heuristic_rules_multiple_urls_variables",
+  "https://github.com/bazelbuild/rules_closure/archive/0.12.0.tar.gz",
+  "7d206c2383811f378a5ef03f4aacbcf5f47fd8650f6abbc3fa89f3a27ddcccc",
+  "0.12.0",
+)
