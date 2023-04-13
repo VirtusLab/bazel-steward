ff8288a08e59068d3cca9b94d5fa212679fb6bf1
Łukasz Wawrzyk <lwawrzyk@virtuslab.com>
2023-04-13 11:04:10 +0200

    Error handling for 404 when computing checksum for bazel rule (#220)
    * Add error handling for sha256 resolution
    
    * Fix PR hooks tests
    
    * apply linter fixes

--- kinds/maven/src/main/kotlin/org/virtuslab/bazelsteward/maven/MavenDataExtractor.kt
+++ kinds/maven/src/main/kotlin/org/virtuslab/bazelsteward/maven/MavenDataExtractor.kt
@@ -1,12 +1,11 @@
 package org.virtuslab.bazelsteward.maven
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.future.await
 import kotlinx.coroutines.withContext
+import org.virtuslab.bazelsteward.core.common.CommandRunner
 import java.nio.file.Path
 
 class MavenDataExtractor(private val workspaceRoot: Path) {
-  private val bazelQuery = "bazel query @maven//:%s --output xml --noshow_progress"
   private val regexPattern = """<source-file location="(.*):1:1" name="@maven//:%s">"""
 
   suspend fun extract(): MavenData {
@@ -19,9 +18,15 @@ class MavenDataExtractor(private val workspaceRoot: Path) {
   }
 
   private suspend fun extractFromFile(fileName: String): List<String> = withContext(Dispatchers.IO) {
-    val process =
-      ProcessBuilder(bazelQuery.format(fileName).split(' ')).directory(workspaceRoot.toFile()).start().onExit().await()
-    val xml = process.inputStream.bufferedReader().use { it.readText() }
+    val xml = CommandRunner.run(
+      workspaceRoot,
+      "bazel",
+      "query",
+      "@maven//:$fileName",
+      "--output",
+      "xml",
+      "--noshow_progress",
+    )
     val fileLocation = Regex(regexPattern.format(fileName)).find(xml)?.let { it.groups[1]?.value }
       ?: throw RuntimeException("Failed to find file: $fileName")
 
