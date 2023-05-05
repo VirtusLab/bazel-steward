23d12cc666fd330567828067865c746edaa63ef9
Łukasz Wawrzyk <lwawrzyk@virtuslab.com>
2023-05-05 15:10:04 +0200

    Fix updating version substrings for rules

--- e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/rules/heuristic/HeuristicUpdateMultipleTest.kt
+++ e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/rules/heuristic/HeuristicUpdateMultipleTest.kt
@@ -3,7 +3,7 @@ package org.virtuslab.bazelsteward.e2e.rules.heuristic
 import org.virtuslab.bazelsteward.e2e.rules.fixture.BazelRulesHeuristicUpdate
 
 class HeuristicUpdateMultipleTest : BazelRulesHeuristicUpdate(
-  "rules/heuristic/heuristic_rules_multiple_urls",
+  "rules/heuristic/heuristic_rules_multiple",
   "https://github.com/bazelbuild/rules_closure/archive/0.12.0.tar.gz",
   "7d206c2383811f378a5ef03f4aacbcf5f47fd8650f6abbc3fa89f3a27ddcccc",
   "0.12.0",
