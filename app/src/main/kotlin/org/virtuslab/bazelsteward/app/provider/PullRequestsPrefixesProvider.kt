9eb44be68142d03d173d9e07d9cc3c127159189a
pkowalczyk <paulina.kowalczyk2@tesco.com>
2023-05-18 14:09:36 +0200

    Add support for configurable branch prefix

--- app/src/main/kotlin/org/virtuslab/bazelsteward/app/provider/PullRequestsPrefixesProvider.kt
+++ app/src/main/kotlin/org/virtuslab/bazelsteward/app/provider/PullRequestsPrefixesProvider.kt
@@ -1,10 +1,13 @@
 package org.virtuslab.bazelsteward.app.provider
 
+import org.virtuslab.bazelsteward.app.BazelStewardGitBranch
 import org.virtuslab.bazelsteward.config.repo.PullRequestsConfig
 
 class PullRequestsPrefixesProvider(private val configs: List<PullRequestsConfig>) {
 
   fun create(): List<String> {
-    return configs.map { it.prefix!! }
+    return configs.filter { it.prefix != null }
+      .map { it.prefix!! }
+      .plus(BazelStewardGitBranch.bazelPrefix)
   }
 }
