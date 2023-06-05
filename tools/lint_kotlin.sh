533eefa205770457cf867fd885205a7b6dace0c9
Łukasz Wawrzyk <lwawrzyk@virtuslab.com>
2023-06-05 16:15:11 +0200

    Improve kotlin lint application

--- tools/lint_kotlin.sh
+++ tools/lint_kotlin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -u -e -o pipefail
-for target in $(bazel query --noshow_progress --output=label "kind('ktlint_fix rule', //...)"); do
+for target in $(bazel test //... --test_tag_filters=lint | grep FAILED | awk '{print $1}' | sed s/_lint_test/_lint_fix/); do
   bazel run "$target"
 done
 
