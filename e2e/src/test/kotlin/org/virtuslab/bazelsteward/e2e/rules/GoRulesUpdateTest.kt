7201eef34189aad9d2fe115d9bcf6e4d87569d2b
Łukasz Wawrzyk <lwawrzyk@virtuslab.com>
2023-06-30 13:19:14 +0200

    Update GoRulesUpdateTest.kt

--- e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/rules/GoRulesUpdateTest.kt
+++ e2e/src/test/kotlin/org/virtuslab/bazelsteward/e2e/rules/GoRulesUpdateTest.kt
@@ -4,5 +4,5 @@ import org.virtuslab.bazelsteward.e2e.rules.fixture.RulesUpdate
 
 class GoRulesUpdateTest : RulesUpdate(
   "rules/rules_go",
-  "rules_go" to "v0.39.1",
+  "rules_go" to "v0.40.0",
 )
